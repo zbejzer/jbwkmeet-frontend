20195e925d9fdc466bc067e10e22b736273f224e
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2025-04-18 15:37:58 +0200

    feat: add carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,24 @@
-import '../styles/style.scss'
+import EmblaCarousel from 'embla-carousel'
+import { addPrevNextBtnsClickHandlers } from './EmblaCarouselArrowButtons'
+
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import '../styles/style.scss'
+import { start } from '@popperjs/core'
+
+const OPTIONS = { slidesToScroll: 1, loop: true, align: 'start' }
+
+const emblaNode = document.querySelector('#photos-carousel')
+const viewportNode = emblaNode.querySelector('.embla__viewport')
+const prevBtnNode = emblaNode.querySelector('.embla__button--prev')
+const nextBtnNode = emblaNode.querySelector('.embla__button--next')
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
+
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+    emblaApi,
+    prevBtnNode,
+    nextBtnNode
+)
+
+emblaApi.on('destroy', removePrevNextBtnsClickHandlers)
