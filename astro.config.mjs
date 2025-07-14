2914778bd5ce638e3885ad419497de6639debbd5
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2025-07-14 08:54:18 +0200

    feat: install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://meet.jbwk.pl',
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
