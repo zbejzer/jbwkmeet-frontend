c7b1ae97b9ba7724da80cb73fdb5fbd9a36957ef
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2025-07-14 08:45:32 +0200

    feat: install Astro sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://meet.jbwk.pl',
+  site: 'https://meet.jbwk.pl',
+  integrations: [sitemap()],
 })
