a1af2c56e9ae7c75fb37122c4208e2a4312d1296
Stanisław Borodziuk <borodziukstanislaw@gmail.com>
2025-07-16 14:24:32 +0200

    style: format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://meet.jbwk.pl',
-  integrations: [sitemap()],
+    site: 'https://meet.jbwk.pl',
+    integrations: [sitemap()],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 })
